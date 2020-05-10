5bc84a7d39e6233d909a10772dbd6dba3b6679b4
Joao Heron <joao.heron@indicium.tech>
2020-05-10 18:06:27 -0300

    added docstrings for model classes

--- model.py
+++ model.py
@@ -2,6 +2,14 @@ import vars
 
 # Base Extractor
 class BaseExtractor():
+    """ 
+    class BaseExtractor
+
+    This class defines a Selenium Extractor.
+
+    Attributes: - download_dir - Allowed inputs: any string path
+                - browser - Allowed inputs: chrome, firefox
+    """
     download_dir = None
     browser = None
 
@@ -12,6 +20,13 @@ class BaseExtractor():
 
 # Base Requester
 class BaseRequester():
+    """ 
+    class BaseRequester
+
+    This class defines a Http Requester.
+
+    Attributes: - download_dir - Allowed inputs: any string path
+    """
     download_dir = None
     
     def __init__(self, download_dir=vars.download_dir):
@@ -19,6 +34,20 @@ class BaseRequester():
 
 # Action
 class Action():
+
+    """ 
+    class Action
+
+    This class defines a webdriver action.
+
+    Attributes: - action_type - Allowed inputs: click, hover, type, drag_and_drop
+                - action_target - Allowed inputs: any selector string
+                - action_selector_kind - Allowed inputs: id, name, xpath, link_text, partial_link_text, tag_name, class_name, css_selector
+                - wait_for - Allowed inputs: any selector string
+                - wait_for_kind - Allowed inputs: id, name, xpath, link_text, partial_link_text, tag_name, class_name, css_selector
+                - type_text - Allowed inputs: any string
+                - timeout - Allowed inputs: any float
+    """
     action_type = None
     action_target = None
     action_selector_kind = None
