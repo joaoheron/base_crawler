4b5d3e0b3379eda9375aaad9def63508bf4b32a6
Joao <joaoheron@gmail.com>
2020-07-05 01:06:49 -0300

    Added CI execution on branch develop

--- deploy/linter.sh
+++ deploy/linter.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-pwd
-ls -la
+
 cd ~
 mkdir .config
 pwd
 ls -la
 
-# python ../opt/atlassian/pipelines/agent/build/deploy_scripts/build_linter_profile.py
-# cd ../opt/atlassian/pipelines/agent/build
+python ../opt/atlassian/pipelines/agent/build/deploy_scripts/build_linter_profile.py
+cd ../opt/atlassian/pipelines/agent/build
 
-# # Type which folders you would like do test with flake8
-# flake8 src/ tests/ deploy/
+# Type which folders you would like do test with flake8
+flake8 app/ tests/ deploy/
 
-# if [ $? -eq 1 ]
-# then
-#     echo "Build has failed because flake8 found errors."
-#     exit 1
-# else
-#     echo "Build has succeeded. Flake8 couldn't found any error. Congratulations!"
-#     exit 0
-# fi
+if [ $? -eq 1 ]
+then
+    echo "Build has failed because flake8 found errors."
+    exit 1
+else
+    echo "Build has succeeded. Flake8 couldn't found any error. Congratulations!"
+    exit 0
+fi
 
